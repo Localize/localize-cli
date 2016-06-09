8f7c4e745b09bd937aaf2ab7e2aa0dfb3fd1b564
Chris Zieba <zieba.chris@gmail.com>
2016-06-08 21:28:52 -0700

    Add push command

---
+++ src/commands.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import sys
+import yaml
+import os 
+import argparse
+import time
+from colorama import init, Fore, Back, Style
+
+def push(opts):
+  print opts
--- src/localize.py
+++ src/localize.py
@@ -1,60 +1,46 @@
 #!/usr/bin/env python
 
-import sys 
+import sys
+import yaml
 import os 
 import argparse
 import time
-import json
-from sys import version_info
-from Parser import Parser
-from Listener import Listener
-from Utils import Utils
-from Connector import Connector
-from watchdog.observers import Observer
 from colorama import init, Fore, Back, Style
+from commands import push
 
-def get_configuration (args):
-  configuration = Parser(args.config)
+def get_configuration(args):
+  with open(args.config, 'r') as ymlfile:
+    cfg = yaml.load(ymlfile)
 
-  # Make sure the watch directory has a trailing slash
-  configuration.watch_dir = os.path.normpath(configuration.watch_dir) + os.sep
+  for section in cfg:
+    print(section)
 
-  return configuration
+  print(cfg['api'])
 
-def command(configuration, reset):
+  return cfg
 
-  if reset == "local":
-    utils.reset_local()
-  elif reset == "remote":
-    utils.reset_remote()
-  else:
-
-  
-  return utils
+def command(opts):
+  push(opts)
 
 
 # Handle any command line arguments
 def parse_args():
   config_required = True
-  config_file = os.getcwd() + "/localize.cfg"
+  config_file = os.getcwd() + "/config.yml"
 
   # Check to see if the default config file exists
   if os.path.isfile(config_file):
     config_required = False
 
   p = argparse.ArgumentParser(description='Localize v0.0.1')
-
+  p.add_argument('push', help='an integer for the accumulator')
   p.add_argument("-c", "--config", help="A configuration file must be present.", required=config_required)
-  p.add_argument("-r", "--reset", help="Options for this argument are [local|remote].", required=False)
+
   args = p.parse_args()
 
   # Add the default config to the args list
   if not config_required:
     args.config = config_file
-
-  # If the reset option was passed 
-  if args.reset and not args.reset in ("local", "remote"):
-    p.error('Options for [-r RESET] are [local|remote].')
   
   return args
 
@@ -66,8 +52,8 @@ def main():
   args = parse_args()
   configuration = get_configuration(args)
 
-  # Run any start up utilities that were passed in on the command line
-  command(configuration, args.reset)
+  # Run the command
+  command(configuration)
 
 if __name__ == '__main__': 
   main()
