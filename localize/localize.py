2d2b6a561c7d88ba1623d5b594176b55f461af4b
Chris Zieba <zieba.chris@gmail.com>
2016-06-17 13:03:47 -0700

    Add the config command

--- localize/localize.py
+++ localize/localize.py
@@ -9,35 +9,45 @@ from colorama import init, Fore, Back, Style
 from commands import *
 
 def get_configuration(args):
-  with open(args.config, 'r') as ymlfile:
+  from os.path import expanduser
+  home = expanduser("~")
+  config_file = home + '/.localize/config.yml'
+
+  # Check to see if the config file exists
+  if not os.path.isfile(config_file):
+    print(Fore.RED + 'No configuration file found! Run the following command to create one:' + Style.RESET_ALL)
+    print('')
+    print('    localize init')
+    print('')
+    print('You can also create the file manually in your $HOME directory: $HOME/.localize/config.yml')
+    print('')
+    sys.exit()
+
+  with open(config_file, 'r') as ymlfile:
     cfg = yaml.load(ymlfile)
 
   return cfg
 
-def command(args, opts):
+def command(args):
+  # Load up the configuration or let the user know to run init
+  if not args.command=='config':
+    configuration = get_configuration(args)
+
   if args.command=='push':
-    push(opts)
+    push(configuration)
   elif args.command=='pull':
-    pull(opts)
+    pull(configuration)
+  elif args.command=='config':
+    config()
+  else:
+    sys.exit(Fore.RED + 'Not a valid command! Did you mean init, push, or pull?' + Style.RESET_ALL)
 
 # Handle any command line arguments
 def parse_args():
-  config_required = True
-  config_file = os.getcwd() + "/config.yml"
-
-  # Check to see if the default config file exists
-  if os.path.isfile(config_file):
-    config_required = False
-
   p = argparse.ArgumentParser(description='Localize v0.0.1')
   p.add_argument('command', nargs='?', help='an integer for the accumulator')
-  p.add_argument("-c", "--config", help="A configuration file must be present.", required=config_required)
 
   args = p.parse_args()
-
-  # Add the default config to the args list
-  if not config_required:
-    args.config = config_file
   
   return args
 
@@ -46,11 +56,13 @@ def main():
   # stdout or stderr, and will replace them with equivalent Win32 calls.
   init(autoreset=True)
 
+  # Checks the command line arguments
   args = parse_args()
-  configuration = get_configuration(args)
-
+  
   # Run the command
-  command(args, configuration)
+  command(args)
+
+
 
 if __name__ == '__main__': 
   main()
