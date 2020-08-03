6f3f4f8bbc87c83606b52262de13a5343fd94f40
DheenathayalanR <dheenathayalan.r@payoda.com>
2020-08-03 11:51:01 +0530

    Changes

--- localize/commands.py
+++ localize/commands.py
@@ -158,4 +158,4 @@ def check_and_return_lang_format(filename, type):
   if filename.count('.') != 1:                      # checking filename, shoud be '<lang>.<format>', for example ru.json, es.csv
     sys.exit(Fore.RED + "Wrong filename for '" + type + "' type, target file have to has the following file format '<language>.<format>', for example ru.json" + Style.RESET_ALL)
   splitted_filename = filename.split('.')           # splitting filename by dot
-  return splitted_filename[0]  # returning language and format
+  return splitted_filename[0]  # returning language
