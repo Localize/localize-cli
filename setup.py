2942ad1da2fcaa49bf05228a1cf0a137143f475d
James Binns <jbinns@localizejs.com>
2021-02-03 11:47:12 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 setup(
   name="localize",
-  version="1.0.6",
+  version="1.0.7",
   author='Localize',
   author_email='support@localizejs.com',
   url='https://help.localizejs.com/docs/localize-cli',
