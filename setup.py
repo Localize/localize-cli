14b0b0f0c672b0a60cb318c89a612fe5b9356164
Jamie Binns <jbinns@localizejs.com>
2021-03-22 16:48:58 -0400

    Update CLI Default Config [DEV-2117] (#23)
    * Updating config to include type
    
    * Updated version and changelog

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 setup(
   name="localize",
-  version="1.0.8",
+  version="1.0.9",
   author='Localize',
   author_email='support@localizejs.com',
   url='https://help.localizejs.com/docs/localize-cli',
