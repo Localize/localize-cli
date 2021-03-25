898538bc71a3be02387481fa983bbccee2ff7150
Shiny Metilda Christina J <shinymetilda.j@payoda.com>
2021-03-25 18:41:30 +0530

    push related test cases

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -6,7 +6,8 @@ import argparse
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../'))
 from localize.localize import *
 from localize.commands import *
-
+project = 'LaHtKc8QETpCK'
+token = 'bfdb2a80531475f2111a5b64d695d214'
 class TestUtils (unittest.TestCase):
 
 	def test_no_command_line_config (self):
@@ -46,30 +47,99 @@ class TestUtils (unittest.TestCase):
 		actual = get_url(config)
 		expected = ('http://localhost:8086/v2.0/projects/somekey/resources')
 		self.assertEqual(actual, expected)
-
-	def test_push (self):
-		print(os.getcwd())
+	
+	def test_push_with_correct_data (self):
 		push_path = os.getcwd() + '/unit/test_files/fr.json'
-		print(push_path)
-		data = dict(
-			api = dict(
-				project = 'LaHtKc8QETpCK',
-				token = 'bfdb2a80531475f2111a5b64d695d214',
-				dev = 'dev',
-			),
-			type = 'phrase',
-			format = 'json',
-			push = dict(
-				sources = [dict(file = push_path)]
-			),
-			pull = dict(
-				targets = [dict(language_code = '/full/path/to/your/file_name.extension')]
-			)
-  		)
+		config = {
+			'api': {
+				'project': project,
+				'token': token,
+				'dev': 'dev',
+			},
+			'format': 'JSON',
+			'push': {
+				'sources': [
+					{ 'file' : push_path },
+				]
+			},
+			'type': 'phrase',
+		}
+		with self.assertRaises(SystemExit):
+			push(config)
+
+	def test_push_with_wrong_format (self):
+		push_path = os.getcwd() + '/unit/test_files/fr.json'
+		config = {
+			'api': {
+				'project': project,
+				'token': token,
+				'dev': 'dev',
+			},
+			'format': 'CSV',
+			'push': {
+				'sources': [
+					{ 'file' : push_path },
+				]
+			},
+			'type': 'phrase',
+		}
 		capturedOutput = StringIO.StringIO()
 		sys.stdout = capturedOutput                 
-		push(data)
+		push(config)
 		sys.stdout = sys.__stdout__
 		actual = capturedOutput.getvalue()
-		expected = 'ValidationError: "type" is required for file'
+		expected = 'File format mismatch for file'
 		self.assertTrue(expected in actual)
+
+	def test_push_missing_token (self):
+		push_path = os.getcwd() + '/unit/test_files/fr.json'
+		config = {
+			'api': {
+				'project': project,
+				'dev': 'dev',
+			},
+			'format': 'JSON',
+			'push': {
+				'sources': [
+					{ 'file' : push_path },
+				]
+			},
+			'type': 'phrase',
+		}
+		with self.assertRaises(KeyError):
+			push(config)
+
+	def test_push_missing_project (self):
+		push_path = os.getcwd() + '/unit/test_files/fr.json'
+		config = {
+			'api': {
+				'token': token,
+				'dev': 'dev',
+			},
+			'format': 'JSON',
+			'push': {
+				'sources': [
+					{ 'file' : push_path },
+				]
+			},
+			'type': 'phrase',
+		}
+		with self.assertRaises(KeyError):
+			push(config)
+
+	def test_push_missing_file (self):
+		config = {
+			'api': {
+				'project': project,
+				'token': token,
+				'dev': 'dev',
+			},
+			'format': 'JSON',
+			'push': {
+				'sources': [
+				]
+			},
+			'type': 'phrase',
+		}
+		with self.assertRaises(SystemExit):
+			push(config)
