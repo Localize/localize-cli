abfb2f37864b64a817e5440dba130f66ed583843
Shiny Metilda Christina J <shinymetilda.j@payoda.com>
2021-03-25 19:47:15 +0530

    staging push

--- tests/unit/test_push.py
+++ tests/unit/test_push.py
@@ -3,105 +3,129 @@ import StringIO
 import sys
 import os
 import argparse
+import test_config
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../'))
 from localize.commands import *
-project = 'LaHtKc8QETpCK'
-token = 'bfdb2a80531475f2111a5b64d695d214'
 
 class TestPush (unittest.TestCase):
 
-	def test_push_with_correct_data (self):
-		push_path = os.getcwd() + '/unit/test_files/fr.json'
-		config = {
-			'api': {
-				'project': project,
-				'token': token,
-				'dev': 'dev',
-			},
-			'format': 'JSON',
-			'push': {
-				'sources': [
-					{ 'file' : push_path },
-				]
-			},
-			'type': 'phrase',
-		}
-		with self.assertRaises(SystemExit):
-			push(config)
+    def test_push_with_correct_data (self):
+        push_path = os.getcwd() + '/unit/test_files/fr.json'
+        config = {
+            'api': {
+                'project': test_config.project,
+                'token': test_config.token,
+            },
+            'format': 'JSON',
+            'push': {
+                'sources': [
+                    { 'file' : push_path },
+                ]
+            },
+            'type': 'phrase',
+        }
+        if test_config.environment is 'dev':
+            config['api']['dev'] = 'dev'
+        else:
+            config['api']['staging'] = 'staging'
 
-	def test_push_with_wrong_format (self):
-		push_path = os.getcwd() + '/unit/test_files/fr.json'
-		config = {
-			'api': {
-				'project': project,
-				'token': token,
-				'dev': 'dev',
-			},
-			'format': 'CSV',
-			'push': {
-				'sources': [
-					{ 'file' : push_path },
-				]
-			},
-			'type': 'phrase',
-		}
-		capturedOutput = StringIO.StringIO()
-		sys.stdout = capturedOutput                 
-		push(config)
-		sys.stdout = sys.__stdout__
-		actual = capturedOutput.getvalue()
-		expected = 'File format mismatch for file'
-		self.assertTrue(expected in actual)
+        with self.assertRaises(SystemExit):
+            push(config)
 
-	def test_push_missing_token (self):
-		push_path = os.getcwd() + '/unit/test_files/fr.json'
-		config = {
-			'api': {
-				'project': project,
-				'dev': 'dev',
-			},
-			'format': 'JSON',
-			'push': {
-				'sources': [
-					{ 'file' : push_path },
-				]
-			},
-			'type': 'phrase',
-		}
-		with self.assertRaises(KeyError):
-			push(config)
+    def test_push_with_wrong_format (self):
+        push_path = os.getcwd() + '/unit/test_files/fr.json'
+        config = {
+            'api': {
+                'project': test_config.project,
+                'token': test_config.token,
+            },
+            'format': 'CSV',
+            'push': {
+                'sources': [
+                    { 'file' : push_path },
+                ]
+            },
+            'type': 'phrase',
+        }
 
-	def test_push_missing_project (self):
-		push_path = os.getcwd() + '/unit/test_files/fr.json'
-		config = {
-			'api': {
-				'token': token,
-				'dev': 'dev',
-			},
-			'format': 'JSON',
-			'push': {
-				'sources': [
-					{ 'file' : push_path },
-				]
-			},
-			'type': 'phrase',
-		}
-		with self.assertRaises(KeyError):
-			push(config)
+        if test_config.environment is 'dev':
+            config['api']['dev'] = 'dev'
+        else:
+            config['api']['staging'] = 'staging'
 
-	def test_push_missing_file (self):
-		config = {
-			'api': {
-				'project': project,
-				'token': token,
-				'dev': 'dev',
-			},
-			'format': 'JSON',
-			'push': {
-				'sources': [
-				]
-			},
-			'type': 'phrase',
-		}
-		with self.assertRaises(SystemExit):
-			push(config)
+        capturedOutput = StringIO.StringIO()
+        sys.stdout = capturedOutput                 
+        push(config)
+        sys.stdout = sys.__stdout__
+        actual = capturedOutput.getvalue()
+        expected = 'File format mismatch for file'
+        self.assertTrue(expected in actual)
+
+    def test_push_missing_token (self):
+        push_path = os.getcwd() + '/unit/test_files/fr.json'
+        config = {
+            'api': {
+                'project': test_config.project,
+            },
+            'format': 'JSON',
+            'push': {
+                'sources': [
+                    { 'file' : push_path },
+                ]
+            },
+            'type': 'phrase',
+        }
+
+        if test_config.environment is 'dev':
+            config['api']['dev'] = 'dev'
+        else:
+            config['api']['staging'] = 'staging'
+
+        with self.assertRaises(KeyError):
+            push(config)
+
+    def test_push_missing_project (self):
+        push_path = os.getcwd() + '/unit/test_files/fr.json'
+        config = {
+            'api': {
+                'token': test_config.token,
+            },
+            'format': 'JSON',
+            'push': {
+                'sources': [
+                    { 'file' : push_path },
+                ]
+            },
+            'type': 'phrase',
+        }
+
+        if test_config.environment is 'dev':
+            config['api']['dev'] = 'dev'
+        else:
+            config['api']['staging'] = 'staging'
+
+        with self.assertRaises(KeyError):
+            push(config)
+
+    def test_push_missing_file (self):
+        config = {
+            'api': {
+                'project': test_config.project,
+                'token': test_config.token,
+                'dev': 'dev',
+            },
+            'format': 'JSON',
+            'push': {
+                'sources': [
+                ]
+            },
+            'type': 'phrase',
+        }
+
+        if test_config.environment is 'dev':
+            config['api']['dev'] = 'dev'
+        else:
+            config['api']['staging'] = 'staging'
+
+        with self.assertRaises(SystemExit):
+            push(config)
