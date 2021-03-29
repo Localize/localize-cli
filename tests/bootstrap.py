45d110613b08a891a29ec66c670e66ee13ef8c80
Shiny Metilda Christina J <shinymetilda.j@payoda.com>
2021-03-29 10:29:40 +0530

    bootstrap file revert

--- tests/bootstrap.py
+++ tests/bootstrap.py
@@ -3,17 +3,17 @@
 import unittest
 import sys
 from unit.test_utils import TestUtils
-# from unit.test_push import TestPush
+from unit.test_push import TestPush
 from unit.test_get_url import TestGetUrl
 from unit.test_pull import TestPull
 
 def main ():
 	# Run the test suites
 	util1= unittest.TestLoader().loadTestsFromTestCase(TestUtils)
-	# util2= unittest.TestLoader().loadTestsFromTestCase(TestPush)
+	util2= unittest.TestLoader().loadTestsFromTestCase(TestPush)
 	util3= unittest.TestLoader().loadTestsFromTestCase(TestGetUrl)
 	util4= unittest.TestLoader().loadTestsFromTestCase(TestPull)
-	unittest.TestSuite([util1, util3, util4])
+	unittest.TestSuite([util1, util2, util3, util4])
 	unittest.main()
 	
 if __name__ == '__main__':
