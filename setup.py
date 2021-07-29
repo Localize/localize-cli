0795f167fec763e22a6967b7f8a10f4f82690385
Jamie Binns <jbinns@localizejs.com>
2021-07-29 17:13:59 -0400

    Tarball didn't compile for v1.1.1, so bumping again and redeploying (#28)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 setup(
   name="localize",
-  version="1.1.0",
+  version="1.1.2",
   author='Localize',
   author_email='support@localizejs.com',
   url='https://help.localizejs.com/docs/localize-cli',
